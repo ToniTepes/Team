834d2815307e3858ebc36e427fb5bee33bd15d5f
Antonio Stacy Foushee <AntonioStacy@gmail.com>
2019-01-12 12:31:36 -0500

    Update Team.js

--- Team.js
+++ Team.js
@@ -1,53 +1,57 @@
 var inquirer = require("inquirer");
+
 function Teammate(name, position, offense, defense) {
-    this.name = name;
-    this.position = position;
-    this.offense = offense;
-    this.defense = defense;
+   this.name = name;
+   this.position = position;
+   this.offense = offense;
+   this.defense = defense;
 }
 
 Teammate.prototype.printInfo = function() {
-    console.log("Name: " + this.name + "\nPosition: " + this.position + "\nOffense: " + 
-    this.offense + "\nDefense: " + this.defense);
-    console.log("_____________________");
+   console.log("Name: " + this.name + "\nPosition: " + this.position +
+   "\nOffense: " + this.offense + "\nDefense: " + this.defense);
+   console.log("---------------");
 };
 
-var count = 0; 
-var team = [];
+var count = 0;
+
+var teammateArray = [];
 
 var teamInput = function() {
-    if ( count < 3) {
-        console.log("New Teammate");
-        inquirer.prompt([
-            {
-                name: "Name",
-                message: "What is your name?"
-              }, {
-                name: "Position",
-                message: "What is your current position?"
-              }, {
-                name: "Offense",
-                message: "Whats your offensive rating?"
-              }, {
-                name: "Deffensive",
-                message: "What is your favorite deffensive rating?"
-              }
-            ]).then(function(answers) {
-                var newTeammate = new Teammate(
-                    answers.name,
-                    answers.position,
-                    answers.offense,
-                    answers.defense,);
-                    team.push(newTeammate);
-                    count++;
-                    teamInput();
-            });
-}
-else {
-    for ( var x = 0; x < team.length; x++) {
-        team[x].printInfo();
-    }
-}
+   if (count < 3) {
+       console.log("NEW TEAMMATE");
+
+       inquirer.prompt([
+          {
+              name: "name",
+              message: "What is your name?"
+          }, {
+               name: "position",
+               message: "What is your current position?"
+          }, {
+               name: "offense",
+               message: "What is your offensive rating?"
+          }, {
+               name: "defense",
+               message: "What is your defensive rating?"
+          }
+       ]).then(function(answers) {
+           var newTeammate = new Teammate(
+               answers.name,
+               answers.position,
+               answers.offense,
+               answers.defense);
+
+           teammateArray.push(newTeammate);
+           count++;
+           teamInput();
+       });
+   }
+   else {
+       for (var i = 0; i < teammateArray.length; i++) {
+           teammateArray[i].printInfo();
+       }
+   }
 };
 
 teamInput();
